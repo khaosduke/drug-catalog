1e94d5a0dd1c09193eec6803d826973fc014ff4c
khaosduke <khaosduke@gmail.com>
2026-07-18 03:27:47 -0400

    Added rust main file

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use clap::Parser;
+
+/// Strips down a DEA controlled substances list to only the relevant columns and removes any entries that are in the exclusion list.
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Main DEA full list, in CSV format
+    #[arg(short, long, default_value = "./input/dea_controlled_substances.csv")]
+    input: String,
+
+    /// Exclusion list directory
+    #[arg(short, long, default_value = "./exclusions")]
+    exclusions: String,
+
+    /// Output directory
+    #[arg(short, long, default_value = "./output/output.csv")]
+    output: String,
+
+}
+
+fn main() {
+    let args = Args::parse();
+
+    println!("Input file: {}", args.input);
+    println!("Exclusions directory: {}", args.exclusions);
+    println!("Output file: {}", args.output);
+}
