03f5b76b6147a4d1e7c25199ae9e7dc9e15d5f87
khaosduke <khaosduke@gmail.com>
2026-08-03 03:38:24 -0400

    implementing map, is able to get rxcui

--- src/map.rs
+++ src/map.rs
@@ -1,8 +1,25 @@
 use rxnorm_api::RxNormApi;
 use serde_json::{Value};
 use std::collections::HashMap;
+use std::fmt::Display;
+use std::fmt;
 
+#[derive(PartialEq)]
+enum ResultType {
+    ExactMatch,
+    ApproximateMatch,
+    NoMatch,
+}
 
+impl Display for ResultType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ResultType::ExactMatch => write!(f, "Exact Match"),
+            ResultType::ApproximateMatch => write!(f, "Approximate Match"),
+            ResultType::NoMatch => write!(f, "No Match"),
+        }
+    }
+}
 pub async fn map(input: &str, output: &str) -> Result<(), Box<dyn std::error::Error>> {
     dea_to_rsxui(input, output).await?;
     Ok(())
@@ -16,7 +33,7 @@ async fn dea_to_rsxui(input: &str, output: &str) -> Result<(), Box<dyn std::erro
 
     let rxnorm = RxNormApi::new()?;
     //Std options for the find rxcui, exact normalized search
-    let mut findrxcui_options = HashMap::from([
+    let mut options = HashMap::from([
         ("format", "json"),
         ("search", "2"),
     ]);
@@ -27,22 +44,43 @@ async fn dea_to_rsxui(input: &str, output: &str) -> Result<(), Box<dyn std::erro
     for result in rdr.records() {
         let record = result?;
         let dea_name = first_token_normalized(&record[0]);
+     
+        let json: Value = loop {
+            let response = rxnorm.find_rxcui_by_string(
+                &dea_name,
+                &options).await?;
 
-        let response = rxnorm.find_rxcui_by_string(
-                &first_token_normalized(&dea_name),
-                &findrxcui_options).await?;
+            let response_body = response.text().await?;
+            let json_response: Value = serde_json::from_str(&response_body)?;
+            
+            //Check the response, if its exact we are done
+            if check_exact_match(&json_response) == ResultType::ExactMatch {
+                break json_response;
+            }
 
-        let response_body = response.text().await?;
-        let json_response: Value = serde_json::from_str(&response_body)?;
+            //If not do an approximate search
+            if let Some(search) = options.get_mut("search") {
+                *search = "9";
+            }
+            let response = rxnorm.find_rxcui_by_string(
+                &dea_name,
+                &options).await?;
+
+            let response_body = response.text().await?;
+            let json_response: Value = serde_json::from_str(&response_body)?;
+
+            break json_response;    
+
+        };
 
         let output_record = [
             &dea_name,
-            &get_rxcui(&json_response).unwrap_or_else(|| "N/A".to_string()),
-            &check_exact_match(&json_response).to_string()
+            &get_rxcui(&json).unwrap_or_else(|| "N/A".to_string()),
+            &check_exact_match(&json).to_string()
         ];
 
 
-        println!("Got: {:?}", json_response["idGroup"]["rxnormId"]);
+        println!("Got: {:?}", json["idGroup"]["rxnormId"]);
         // Write the filtered record to the output file
         wtr.write_record(&output_record)?;
     }
@@ -64,18 +102,18 @@ fn get_rxcui(json_response: &Value) -> Option<String> {
     Some(rxnorm_ids[0].as_str().unwrap_or("").to_string())
 }
 
-fn check_exact_match(json_response: &Value) -> bool {
+fn check_exact_match(json_response: &Value) -> ResultType {
     //Get the array
     if json_response["idGroup"]["rxnormId"].is_null() {
-        return false;
+        return ResultType::NoMatch;
     }
 
     let rxnorm_ids = json_response["idGroup"]["rxnormId"].as_array().unwrap();
-    //If we get two ideas we dont have exact match 
+    //If we get two ids we dont have exact match 
     if rxnorm_ids.len() > 1 {
-        return false;
+        return ResultType::ApproximateMatch;
     }
-    true
+    ResultType::ExactMatch
 }
 
 //Just in case we get a drug name with a chemical name following
